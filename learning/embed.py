01b790ba0e1a72cbfb3f9a54826aae847896a8fa
Kavan Sikand <kvsikand@gmail.com>
2019-11-19 15:28:42 -0600

    no grad during eval

--- learning/embed.py
+++ learning/embed.py
@@ -19,11 +19,9 @@ opt = parser.parse_args()
 model = PointNetLC()
 model.load_state_dict(torch.load(opt.model))
 model.eval()
-
-# TODO: do dataset.py stuff
-
-f = opt.data_path
-point_set = torch.tensor([get_point_cloud_from_file(f)])
-point_set = point_set.transpose(2, 1)
-embedding, trans, _ = model.forward(point_set)
-np.savetxt(opt.out_path, embedding.detach().numpy())
+with torch.no_grad():
+    f = opt.data_path
+    point_set = torch.tensor([get_point_cloud_from_file(f)])
+    point_set = point_set.transpose(2, 1)
+    embedding, trans, _ = model.forward(point_set)
+    np.savetxt(opt.out_path, embedding.detach().numpy())
