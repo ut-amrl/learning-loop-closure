320e0f0e8e6092b6d13a67acab59f4becc5f01a1
Kavan Sikand <kvsikand@gmail.com>
2019-11-28 00:19:44 -0600

    take 1

--- learning/model.py
+++ learning/model.py
@@ -3,58 +3,36 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-
-class PointNetSiamese(nn.Module):
+class S2Net(nn.Module):
     def __init__(self):
-        super(PointNetSiamese, self).__init__()
-        self.embedding = PointNetLC()
-        self.dropout = nn.Dropout(0.15)
-        self.ff = nn.Linear(512, 2)
-        self.softmax = nn.LogSoftmax(dim=1)
-        nn.init.xavier_uniform_(self.ff.weight)
-    
-    def forward(self, x, y):
-        first_emb, _, _ = self.embedding(x)
-        second_emb, _, _ = self.embedding(y)
+        super(S2Net, self).__init__()
+        self.translate = torch.zeros((2, 1))
+        self.theta = Variable(0)
 
-        scores = self.ff(self.dropout(torch.cat([first_emb, second_emb], 1)))
-
-        return self.softmax(scores)
-
-
-class PointNetLC(nn.Module):
-    def __init__(self):
-        super(PointNetLC, self).__init__()
-        self.feat = PointNetfeat2d(feature_transform=True)
-        self.ff = nn.Linear(256, 256, True)
-        nn.init.xavier_uniform_(self.ff.weight)
-
+    # TODO handle batches
     def forward(self, x):
-        global_vec, trans, trans_feat = self.feat(x)
+        # construct rotation
+        s = torch.sin(self.theta)
+        c = torch.cos(self.theta)
+        rotation = torch.tensor([
+            [c, s],
+            [-s, c]
+        ])
+        return torch.mm(rotation, x) + self.translate
 
-        # feed forward layer just to adjust the global vec.
-        out_vec = self.ff(global_vec)
-
-        return out_vec, trans, trans_feat
-
-
-class PointNetfeat2d(nn.Module):
-    def __init__(self, feature_transform=False):
-        super(PointNetfeat2d, self).__init__()
-        self.stn = STNkd(2)
+class EmbeddingNet(nn.Module):
+    def __init__(self):
+        super(EmbeddingNet, self).__init__()
+        self.transform = S2Net()
         self.conv1 = torch.nn.Conv1d(2, 32, 1)
-        self.conv2 = torch.nn.Conv1d(32, 64, 1)
-        self.conv3 = torch.nn.Conv1d(64, 256, 1)
+        self.conv2 = torch.nn.Conv1d(32, 128, 1)
         self.bn1 = nn.BatchNorm1d(32)
-        self.bn2 = nn.BatchNorm1d(64)
-        self.bn3 = nn.BatchNorm1d(256)
-        self.feature_transform = feature_transform
-        if self.feature_transform:
-            self.fstn = STNkd(k=32)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.fstn = STNkd(k=32)
 
     def forward(self, x):
         n_pts = x.size()[2]
-        trans = self.stn(x)
+        trans = self.transform(x)
         x = x.transpose(2, 1)
         x = torch.bmm(x, trans)
         x = x.transpose(2, 1)
@@ -68,8 +46,24 @@ class PointNetfeat2d(nn.Module):
         else:
             trans_feat = None
 
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = self.bn3(self.conv3(x))
+        x = self.bn2(self.conv2(x))
         x = torch.max(x, 2, keepdim=True)[0]
-        x = x.view(-1, 256)
+        x = x.view(-1, 128)
         return x, trans, trans_feat
+
+class FullNet(nn.Module):
+    def __init__(self):
+        super(FullNet, self).__init__()
+        self.embedding = EmbeddingNet()
+        self.ff = nn.Linear(256, 2)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x, y):
+        x_emb, x_trans, _ = self.embedding(x)
+        y_emb, y_trans, _ = self.embedding(y)
+
+        scores = self.ff(torch.cat(x_emb, y_emb))
+
+        out = self.LogSoftmax(scores)
+
+        out, x_trans, y_trans
