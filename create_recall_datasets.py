d309abdaee0dd958ed67004841022b7166bf179c
Kavan Sikand <kvsikand@gmail.com>
2020-01-16 15:24:40 -0600

    finish recall evaluation logic

--- create_recall_datasets.py
+++ create_recall_datasets.py
@@ -39,6 +39,12 @@ def save_target_locations():
     with open(location_file, 'w') as f:
         f.write(loc_info)
 
+def save_dataset_info():
+    dataset_file = os.path.join(out_dir, 'datasets.json')
+    ds_info = json.dumps([args.first_dataset, args.second_dataset])
+    with open(dataset_file, 'w') as f:
+        f.write(ds_info)
+
 def handle_location_input(publisher):
     def handle_input(data):
         print("Recieved Input from localization gui", data)
@@ -62,6 +68,8 @@ location_listener()
 save_target_locations()
 print('Saved target location information')
 
+save_dataset_info()
+
 # Now, find the locations in the datasets that are close to this spot.
 
 def load_locations(ds_dir):
@@ -75,7 +83,7 @@ def load_locations(ds_dir):
 
 def load_scan_files(ds_dir):
     scan_files = glob.glob(os.path.join(ds_dir, 'point_*.npy'))
-    scan_timestamps = np.asarray([s[s.find('point_') + len('point_'):s.find('.npy')] for s in scan_files]).astype(np.float32)
+    scan_timestamps = np.asarray([s[s.find('point_') + len('point_'):s.find('.npy')] for s in scan_files])
 
     return scan_timestamps, scan_files
 
@@ -91,10 +99,11 @@ ds2_time_tree = cKDTree(np.asarray([d[:2] for d in ds2_locations]))
 ds1_scan_timestamps, ds1_scan_files = load_scan_files(args.first_dataset)
 ds2_scan_timestamps, ds2_scan_files = load_scan_files(args.second_dataset)
 
+
 ds1_scan_tree = cKDTree([[ts,] for ts in ds1_scan_timestamps])
 ds2_scan_tree = cKDTree([[ts,] for ts in ds2_scan_timestamps])
 
-THRESHOLD = 0.85
+THRESHOLD = 0.8
 
 def check_overlap(target_loc, locations):
     def checker(loc_idx):
@@ -104,9 +113,9 @@ def check_overlap(target_loc, locations):
     return checker
 
 # For each target location, find a fixed number of nearby observations in each dataset
-obs_per_target = 5
+obs_per_target = 3
 observation_loc_timestamps = np.ndarray((len(target_locations), 2, obs_per_target, 1))
-observation_scan_timestamps = np.ndarray((len(target_locations), 2, obs_per_target, 1))
+observation_scan_files = np.ndarray((len(target_locations), 2, obs_per_target, 1))
 for idx, target_loc in enumerate(target_locations):
     ds1_neighbors = ds1_time_tree.query_ball_point(target_loc[:2], 1)
     filtered_ds1 = np.asarray(list(filter(check_overlap(target_loc, ds1_locations), ds1_neighbors)))
@@ -120,11 +129,10 @@ for idx, target_loc in enumerate(target_locations):
     
     observation_loc_timestamps[idx, 0] = [[ts,] for ts in ds1_timestamps[filtered_ds1]]
     observation_loc_timestamps[idx, 1] = [[ts,] for ts in ds2_timestamps[filtered_ds2]]
-
-    import pdb; pdb.set_trace()
+    
     closest_ds1_timestamps = ds1_scan_tree.query(observation_loc_timestamps[idx, 0])[1]
     closest_ds2_timestamps = ds2_scan_tree.query(observation_loc_timestamps[idx, 1])[1]
-    observation_scan_timestamps[idx, 0] = ds1_scan_timestamps[closest_ds1_timestamps]
-    observation_scan_timestamps[idx, 1] = ds2_scan_timestamps[closest_ds2_timestamps]
+    observation_scan_files[idx, 0] = [[ts,] for ts in ds1_scan_timestamps[closest_ds1_timestamps]]
+    observation_scan_files[idx, 1] = [[ts,] for ts in ds2_scan_timestamps[closest_ds2_timestamps]]
 
-np.save(os.path.join(out_dir, 'observation_timestamps.npy'), observation_scan_timestamps)
+np.save(os.path.join(out_dir, 'observation_timestamps.npy'), observation_scan_files)
--- evaluate_recall.py
+++ evaluate_recall.py
@@ -6,6 +6,7 @@ import torch
 import os
 from learning.train_helpers import create_embedder
 from helpers import scan_to_point_cloud, LCBagDataReader, embedding_for_scan
+import json
 
 parser = argparse.ArgumentParser(
     description='Find loop closure locations for some ROS bag')
@@ -18,11 +19,31 @@ args = parser.parse_args()
 model = create_embedder(args.model)
 model.eval()
 
+ds_file = os.path.join(args.recall_dataset, 'datasets.json')
+with open(ds_file) as f:
+    dataset_names = json.loads(f.read())
+
+def load_cloud(ds, timestamp):
+    dataset_name = dataset_names[ds]
+    cloud_data = os.path.join(dataset_name, 'point_{0}.npy'.format(timestamp))
+    return np.load(cloud_data)
+
 # recall location x dataset x timestamp
 obs_timestamps = np.load(os.path.join(args.recall_dataset, 'observation_timestamps.npy'))
 
-
 # We want to assert that for every recall location, each scan in dataset 0 matches each scan in dataset 1
-for loc_idx in obs_timestamps.shape[0]:
-    first_dataset_timestamps = obs_timestamps[loc_idx][0]
-    second_dataset_timestamps = obs_timestamps[loc_idx][1]
+for loc_idx in range(obs_timestamps.shape[0]):
+    first_dataset_timestamps = obs_timestamps[loc_idx, 0]
+    second_dataset_timestamps = obs_timestamps[loc_idx, 1]
+    import pdb; pdb.set_trace()
+    for ds1_timestamp in first_dataset_timestamps:
+        for ds2_timestamp in second_dataset_timestamps:
+            first_cloud = load_cloud(0, ds1_timestamp[0])
+            second_cloud = load_cloud(1, ds2_timestamp[0])
+
+            emb1, _, _, _ = embedding_for_scan(model, first_cloud)
+            emb2, _, _, _ = embedding_for_scan(model, second_cloud)
+            
+            distance = torch.norm(emb1 - emb2, p=2, dim=0)
+
+            print('distance', distance)
