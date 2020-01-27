fix normalization, add axes to plotting, etc

helpers.py
@@ -78,20 +78,20 @@ def scan_to_point_cloud(scan, trim_edges=True):
             cloud[idx][1] = point[1]
         angle_offset += scan.angle_increment
 
+    cloud = normalize_point_cloud(cloud, scan.range_max)
+
     return cloud
 
-def normalize_point_cloud(point_set):
+def normalize_point_cloud(point_set, max_range):
     point_set = point_set - \
         np.expand_dims(np.mean(point_set, axis=0), 0)  # center
     point_set = np.delete(point_set, 2, axis=1)
     # normalize
-    dist = np.max(np.sqrt(np.sum(point_set ** 2, axis=1)), 0)
-    point_set = point_set / dist  # scale
+    point_set = point_set / max_range  # scale
     return point_set
 
-def embedding_for_scan(model, scan):
-    normalized_cloud = normalize_point_cloud(scan)
-    cloud = torch.tensor([normalized_cloud])
+def embedding_for_scan(model, cloud):
+    cloud = torch.tensor([cloud])
     cloud = cloud.transpose(2, 1)
     cloud = cloud.cuda()
     result = model(cloud)

learning/dataset.py
@@ -15,14 +15,14 @@ from plyfile import PlyData, PlyElement
 import pickle
 
 sys.path.append(os.path.join(os.getcwd(), '..'))
-from helpers import compute_overlap, normalize_point_cloud
+from helpers import compute_overlap
 
 CLOSE_DISTANCE_THRESHOLD = 1
 OVERLAP_THRESHOLD = 0.75
 
 def get_point_cloud_from_file(filename):
     point_set = np.load(filename).astype(np.float32)
-    return normalize_point_cloud(point_set)
+    return point_set
 
 class LCDataset(data.Dataset):
     def __init__(self,
@@ -76,6 +76,7 @@ class LCTripletDataset(data.Dataset):
         # self.overlap_radius = self.dataset_info['scanMetadata']['range_max'] * 0.4 if 'scanMetadata' in self.dataset_info else 4
         self.data_loaded = False
         self.triplets_loaded = False
+        self.computed_new_distances = False
         self.data = []
         self.overlaps = {}
         self.triplets = []
@@ -165,6 +166,7 @@ class LCTripletDataset(data.Dataset):
                 return self.overlaps[key] > OVERLAP_THRESHOLD
             else:
                 overlap = compute_overlap(location, alt_loc)
+                self.computed_new_distances = True
                 self.overlaps[key] = overlap
                 return self.overlaps[key] > OVERLAP_THRESHOLD
 