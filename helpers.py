fix neighbor/distant computation

helpers.py
@@ -9,7 +9,6 @@ FOV = np.pi * 3 / 2
 RADIUS = 3
 SAMPLE_RESOLUTION = 6
 
-
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
@@ -22,7 +21,7 @@ def visualize_location(plt, location, color='blue'):
 def visualize_cloud(plt, cloud, color='blue'):
     plt.xlim(-1, 1)
     plt.ylim(-1, 1)
-    plt.scatter(cloud[:, 0], cloud[:, 1], c='blue', marker='.')
+    plt.scatter(cloud[:, 0], cloud[:, 1], c=color, marker='.')
 
 def arc_patch(center, radius, theta1, theta2, ax=None, resolution=50, **kwargs):
     # make sure ax is not empty

visualize_triplets.py
@@ -47,10 +47,10 @@ for i in range(triplets.shape[0]):
         # get first in batch
         triplet = batch_triplets[j]
 
-        anchor_np, anchor_loc, _ = dataset.get_by_timestamp(triplet[0, 0], include_angle=True)
+        anchor_np, anchor_loc, ts = dataset.get_by_timestamp(triplet[0, 0], include_angle=True)
         similar_np, similar_loc, _ = dataset.get_by_timestamp(triplet[1, 0], include_angle=True)
         distant_np, distant_loc, _ = dataset.get_by_timestamp(triplet[2, 0], include_angle=True)
-
+        print("Anchor Timestamp", ts)
         print("Locations", anchor_loc, similar_loc, distant_loc)
 
         anchor = torch.tensor(anchor_np.transpose(1, 0)).unsqueeze(0).cuda()