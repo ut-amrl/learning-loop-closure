447d8db37ab0a3a0fc3623861de28ab357c5509b
Kavan Sikand <kvsikand@gmail.com>
2020-01-30 23:32:58 -0600

    update model, more layers

--- learning/model.py
+++ learning/model.py
@@ -56,18 +56,25 @@ class EmbeddingNet(nn.Module):
     def __init__(self):
         super(EmbeddingNet, self).__init__()
         self.transform = TransformNet()
-        self.conv1 = torch.nn.Conv1d(2, 16, 1)
-        self.conv2 = torch.nn.Conv1d(16, 32, 1)
+        self.conv1 = torch.nn.Conv1d(2, 8, 1)
+        self.conv2 = torch.nn.Conv1d(8, 16, 1)
+        self.conv3 = torch.nn.Conv1d(16, 32, 1)
+        self.conv4 = torch.nn.Conv1d(32, 32, 1)
         self.ff = nn.Linear(32, 16)
-        self.bn1 = nn.BatchNorm1d(16)
-        self.bn2 = nn.BatchNorm1d(32)
+        self.bn1 = nn.BatchNorm1d(8)
+        self.bn2 = nn.BatchNorm1d(16)
+        self.bn3 = nn.BatchNorm1d(32)
+        self.bn4 = nn.BatchNorm1d(32)
         nn.init.xavier_uniform_(self.ff.weight)
 
     def forward(self, x):
         x, translation, theta = self.transform(x)
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.bn1(self.conv1(x))
+        x = self.bn2(self.conv2(x))
+        x = self.bn3(self.conv3(x))
+        x = self.bn4(self.conv4(x))
         x = F.max_pool1d(x, x.shape[2])
+        x = F.relu(x)
         x = self.ff(x.transpose(2, 1)).transpose(2, 1).squeeze(-1)
         return x, translation, theta
 
