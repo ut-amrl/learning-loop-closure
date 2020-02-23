7e5ca5b22e1454606c739a042bd403868a6d13a5
Kavan Sikand <kvsikand@gmail.com>
2020-02-23 15:49:49 -0600

    finish up dataset combination

--- generate_lcc_dataset.py
+++ generate_lcc_dataset.py
@@ -1,11 +1,13 @@
 
 import argparse
 import os
- import sys
+import sys
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, required=True, help="source dataset ")
 parser.add_argument('--combine_rule', type=str, default='and', help="one of (and, or)")
+parser.add_argument('--out_file', type=str, default='lcc_timestamps', required=True, help="name of output file to store this timestamp info in (default: lcc_timestamps)")
 parser.add_argument('--labeled_timestamps', type=str, nargs='+' required=True, help="paths to timestamp labels to join together")
 opt = parser.parse_args()
 start_time = str(int(time.time()))
@@ -33,3 +35,7 @@ for dataset in opt.labeled_timestamps:
                 raise Error('Invalid combine rule')
         else:
             all_labeled[true_ts] = label
+
+labeled_dataset = np.array(list(all_labeled.items()), dtype=np.float32)
+
+np.save(opt.out_file + '.npy', labeled_dataset)
