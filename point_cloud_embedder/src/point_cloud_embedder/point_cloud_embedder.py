e6b95625697ebc42f0bf4e1c8a23626b7589d20f
Kavan Sikand <kvsikand@gmail.com>
2020-03-11 22:46:28 -0500

    more progress, client

--- point_cloud_embedder/src/point_cloud_embedder/point_cloud_embedder.py
+++ point_cloud_embedder/src/point_cloud_embedder/point_cloud_embedder.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
+from sensor_msgs import point_cloud2
 from sensor_msgs.msg import PointCloud2
 from srv import GetPointCloudEmbedding, GetPointCloudEmbeddingResponse
 import sys
@@ -8,7 +9,6 @@ from os import path
 
 # TODO fix this hack
 sys.path.append(path.dirname(path.dirname(path.dirname( path.dirname( path.abspath(__file__)  ) ) )))
-print(sys.path)
 from helpers import embedding_for_scan
 from learning.train_helpers import create_embedder
 
@@ -16,9 +16,10 @@ def create_embed_helper(embedder):
   def embed_cloud(req):
     cloud = req.cloud
     cloud_np = []
-    for point in sensor_msgs.point_cloud2.read_points(req.cloud, skip_nans=True):
-      clouds_np.append([point[0], point[1]])
-    clouds_np = np.array(clouds_np).astype(np.float32)
+    import pdb; pdb.set_trace()
+    for point in point_cloud2.read_points(req.cloud, skip_nans=True):
+      cloud_np.append([point[0], point[1]])
+    cloud_np = np.array(cloud_np).astype(np.float32)
     embedding = embedding_for_scan(embedder, cloud)[0].to_numpy()
     return GetPointCloudEmbeddingResponse(embedding)
   return embed_cloud
@@ -26,11 +27,11 @@ def create_embed_helper(embedder):
 
 def service():
   rospy.init_node('point_cloud_embedder', anonymous=True)
-  # embedding_model = rospy.get_param('embedding_model')
-  print(rospy.get_param_names())
-  embedder = create_embedder()
+  embedding_model = sys.argv[1]
+  embedder = create_embedder(embedding_model)
   service = rospy.Service('embed_point_cloud', GetPointCloudEmbedding, create_embed_helper(embedder), buff_size=2)
-
+  rospy.spin()
+  
 if __name__ == '__main__':
   try:
     service()
