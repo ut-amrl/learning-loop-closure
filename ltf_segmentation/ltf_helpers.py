6ca445646cd89429430ee147a2d2a10ac27948f3
Kavan Sikand <kvsikand@gmail.com>
2020-03-15 12:45:02 -0500

    clean up discretizinog

--- ltf_segmentation/ltf_helpers.py
+++ ltf_segmentation/ltf_helpers.py
@@ -1,11 +1,5 @@
 import numpy as np
-import sys
-import os
 import math
-cloud = np.load('../data/gdc1_cobot/point_125.12109.npy')
-
-sys.path.append(os.path.join(os.getcwd(), '..'))
-from helpers import normalize_point_cloud
 
 # Takes an input point cloud and discretizes it into an occupancy grid of size (dimensions x dimensions)
 # Assumes the input point cloud is normalized to [-1, 1]
@@ -17,9 +11,3 @@ def discretize_point_cloud(cloud, dimensions):
     discretized[cell] = 1
   
   return discretized
-
-
-discretized = discretize_point_cloud(cloud, 200)
-from matplotlib import pyplot as plt
-plt.imshow(discretized, interpolation='nearest')
-plt.show()
