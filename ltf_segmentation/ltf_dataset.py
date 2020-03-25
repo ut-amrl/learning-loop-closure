245f180f22a80b1174b57a7864794b96dc1c865c
Kavan Sikand <kvsikand@gmail.com>
2020-03-25 02:04:30 -0500

    ltf training

--- ltf_segmentation/ltf_dataset.py
+++ ltf_segmentation/ltf_dataset.py
@@ -1,7 +1,7 @@
 import rosbag
 from tqdm import tqdm
 from ltf_helpers import discretize_point_cloud
-
+import numpy as np
 import sys
 import os
 
@@ -9,7 +9,7 @@ sys.path.append(os.path.join(os.getcwd(), '..'))
 from data_processing.data_processing_helpers import scan_to_point_cloud
 
 class LTFDataset:
-  def __init__(self, bag_file, base_topic, filtered_topic, dimensions=200, lidar_range=20):
+  def __init__(self, bag_file, base_topic, filtered_topic, dimensions=200, lidar_range=30):
     self.bag_file = bag_file
     self.base_topic = base_topic
     self.filtered_topic = filtered_topic
@@ -23,17 +23,21 @@ class LTFDataset:
     self.data = {}
     start_time = bag.get_start_time()
     for topic, msg, t in tqdm(bag.read_messages(topics=[self.base_topic, self.filtered_topic])):
-        timestamp = t.secs + t.nsecs * 1e-9 - start_time
+        if len(timestamps) > 500:
+          break
+        timestamp = round(t.secs + t.nsecs * 1e-9 - start_time, 5)
         timestamps.add(timestamp)
         if timestamp not in self.data:
-          self.data[timestamp] = [{}, {}]
+          self.data[timestamp] = [np.zeros((3, self.dimensions, self.dimensions)).astype(np.float32), np.zeros((self.dimensions, self.dimensions)).astype(np.int)]
         if (topic == self.base_topic):
-          self.data[timestamp][0] = discretize_point_cloud(scan_to_point_cloud(msg), self.lidar_range, self.dimensions)
+          self.data[timestamp][0] = np.rollaxis(discretize_point_cloud(scan_to_point_cloud(msg), self.lidar_range, self.dimensions, True), 2, 0)
         elif (topic == self.filtered_topic):
-          
           self.data[timestamp][1] = discretize_point_cloud(scan_to_point_cloud(msg), self.lidar_range, self.dimensions)
     
     self.timestamps = sorted(list(timestamps))
 
   def __getitem__(self, index):
-    return self.data[self.timestamps[index]]
+    return self.data[self.timestamps[index]][0], self.data[self.timestamps[index]][1]
+
+  def __len__(self,):
+    return len(self.timestamps)
--- ltf_segmentation/train_ltf.py
+++ ltf_segmentation/train_ltf.py
@@ -19,7 +19,7 @@ from helpers import print_output, initialize_logging, save_model
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '--batch_size', type=int, default=2, help='input batch size')
+    '--batch_size', type=int, default=64, help='input batch size')
 parser.add_argument(
     '--workers', type=int, help='number of data loading workers', default=4)
 parser.add_argument(
@@ -47,8 +47,6 @@ torch.manual_seed(opt.manualSeed)
 
 
 dataset = LTFDataset(opt.bag_file, opt.base_topic, opt.filtered_topic, 200)
-dataset.load_data()
-
 out_dir = opt.outf + '_' + os.path.basename(opt.bag_file)
 
 try:
@@ -56,36 +54,29 @@ try:
 except OSError:
     pass
 
-ltf_model = SegNet()
+dataloader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=opt.batch_size,
+    shuffle=True,
+    num_workers=num_workers,
+    drop_last=True)
+
+ltf_model = SegNet(3, 2).cuda()
 
 optimizer = optim.Adam(ltf_model.parameters(), lr=1e-3, weight_decay=1e-5)
+lossFunc = torch.nn.CrossEntropyLoss()
 
 print_output("Press 'return' at any time to finish training after the current epoch.")
 for epoch in range(opt.nepoch):
     total_loss = 0
 
     for i, data in tqdm(enumerate(dataloader, 0)):
-        ((clouds, locations, _), (similar_clouds, similar_locs, _), (distant_clouds, distant_locs, _)) = data
-        clouds = clouds.transpose(2, 1)
-        similar_clouds = similar_clouds.transpose(2, 1)
-        distant_clouds = distant_clouds.transpose(2, 1)
+        original, filtered = data
+
+        generated = ltf_model(original.cuda())[0]
         
-        clouds = clouds.cuda()
-        similar_clouds = similar_clouds.cuda()
-        distant_clouds = distant_clouds.cuda()
-
-        optimizer.zero_grad()
-        embedder.zero_grad()
-
-        anchor_embeddings, trans, theta = embedder(clouds)
-        similar_embeddings, sim_trans, sim_theta = embedder(similar_clouds)
-        distant_embeddings, dist_trans, dist_theta = embedder(distant_clouds)
-
-        total_similar_dist += torch.sum(torch.norm(anchor_embeddings - similar_embeddings, dim=1)) 
-        total_distant_dist += torch.sum(torch.norm(anchor_embeddings - distant_embeddings, dim=1)) 
-
         # Compute loss here
-        loss = lossFunc.forward(anchor_embeddings, similar_embeddings, distant_embeddings)
+        loss = lossFunc.forward(generated, filtered.cuda())
 
         loss.backward()
         optimizer.step()
