2af7a5ccbb75fcbdf22bce77e72a1221bbf76495
Kavan Sikand <kvsikand@gmail.com>
2020-05-13 10:17:00 -0500

    add interactive

--- evaluation/visualize_lc_trajectory.py
+++ evaluation/visualize_lc_trajectory.py
@@ -16,6 +16,7 @@ from config import Configuration
 config = Configuration(False, True)
 config.add_argument('--alt_bag_file', type=str)
 config.add_argument('--time_spacing', type=float, default=1.5)
+config.add_argument('--interactive', action='store_true')
 config = config.parse()
 
 scan_conv, scan_match, scan_transform = helpers.create_laser_networks(config.model_dir, config.model_epoch)
@@ -57,24 +58,7 @@ ax = fig.add_subplot(111, projection='3d')
 ax.plot(base_trajectory[:, 0], base_trajectory[:, 1], 0, color='blue')
 ax.plot(target_trajectory[:, 0], target_trajectory[:, 1], 5, color='green')
 
-fp = 0
-tp = 0
-# Now find loop closures along these trajectories
-for base_idx in np.linspace(0, len(bag_reader.get_localization_timestamps()) - 1, 50):      
-  # loc = bag_reader.get_localizations()[bag_reader.get_localization_timestamps()[base_idx]]
-  # map_fig = plt.figure()
-
-  # if opt.map_name:
-  #   evaluation_helpers.draw_map(map_fig, opt.map_name)
-  #   evaluation_helpers.visualize_location(map_fig, loc)
-  
-  #   plt.show(block=False)
-
-  # response = raw_input('Press y to see the full plot for this index')
-  # plt.close()
-
-  # if response == 'y':
-  base_idx = int(base_idx)
+def add_matches_for_index(base_idx):
   base_timestamp = bag_reader.get_localization_timestamps()[base_idx]
   base_scan = torch.tensor(bag_reader.get_closest_scan_by_time(base_timestamp)[0].ranges).cuda()
 
@@ -89,19 +73,41 @@ for base_idx in np.linspace(0, len(bag_reader.get_localization_timestamps()) - 1
       if np.linalg.norm(base_trajectory[base_idx] - target_trajectory[idx]) < 2.5:
         tp += 1
         c = 'gray'
+        if (config.bag_file == config.alt_bag_file):
+          if (abs(base_idx - target_trajectory) > 5):
+            c = 'green'
       else:
         fp +=1 
         c = 'red'
       ax.plot([base_trajectory[base_idx, 0], target_trajectory[idx, 0]], [base_trajectory[base_idx, 1], target_trajectory[idx, 1]], [0, 5], color=c)
 
-info_string = "True Positives: {0}\tFalse Positives: {1}".format(tp, fp)
+def finish_and_show_plot():
+  info_string = "True Positives: {0}\tFalse Positives: {1}".format(tp, fp)
 
-legend_elements = [
-  plt.Line2D([0], [0], color='gray', lw=4, label='Correct Match ({0})'.format(tp)),
-  Line2D([0], [0], color='red', lw=4, label='Incorrect Match ({0})'.format(fp)),
-]
+  legend_elements = [
+    plt.Line2D([0], [0], color='gray', lw=4, label='Correct Match ({0})'.format(tp)),
+    Line2D([0], [0], color='red', lw=4, label='Incorrect Match ({0})'.format(fp)),
+  ]
 
-ax.legend(handles=legend_elements)
-print(info_string)
-# plt.text(10, 10, info_string)
-plt.show()
+  ax.legend(handles=legend_elements)
+  print(info_string)
+  # plt.text(10, 10, info_string)
+  plt.show()
+
+if interactive:
+  while True:
+    idx = input("Enter index:")
+    fp = 0
+    tp = 0
+    finish_and_show_plot()
+
+# Now find loop closures along these trajectories
+else:
+  fp = 0
+  tp = 0
+  for base_idx in np.linspace(0, len(bag_reader.get_localization_timestamps()) - 1, 50):
+    # if response == 'y':
+    base_idx = int(base_idx)
+    add_matches_for_index(base_idx)
+
+  finish_and_show_plot()
